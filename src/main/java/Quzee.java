6e299c1b2be3e733fff1002bfa02fe16b0f7e094
Lee Qi Zheng <leeqz2001@gmail.com>
2026-01-20 18:15:51 +0800

    Rename Duke to Quzee

---
+++ src/main/java/Quzee.java
@@ -0,0 +1,9 @@
+public class Quzee {
+
+    public static final String CHATBOT_NAME = "Quzee";
+
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm " + CHATBOT_NAME + "\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
