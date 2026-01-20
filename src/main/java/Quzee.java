f2e9c581dfa874129f3933540a2a92219117da52
Lee Qi Zheng <leeqz2001@gmail.com>
2026-01-20 18:47:33 +0800

    Modify Quezee.java with echo function

--- src/main/java/Quzee.java
+++ src/main/java/Quzee.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 public class Quzee {
 
     public static final String CHATBOT_NAME = "Quzee";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm " + CHATBOT_NAME + "\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!\n");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+
+        }
+
     }
 }
