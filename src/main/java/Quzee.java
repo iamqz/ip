fbc7e3ed526b9d2c777c30624c5aee3937245c86
Lee Qi Zheng <leeqz2001@gmail.com>
2026-01-22 14:25:28 +0800

    Modify Quzee.java: implement add and list features

--- src/main/java/Quzee.java
+++ src/main/java/Quzee.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Quzee {
 
     public static final String CHATBOT_NAME = "Quzee";
+    public static ArrayList<String> database = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,8 +14,15 @@ public class Quzee {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
+            } else if (userInput.equals("list")) {
+                int count = 0;
+                for (String s : database) {
+                    System.out.println(++count + ". " + s);
+                }
+                System.out.println("\n");
             } else {
-                System.out.println(userInput);
+                database.add(userInput);
+                System.out.println("added: " + userInput + "\n");
             }
 
         }
