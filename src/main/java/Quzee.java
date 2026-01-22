58674afbf724b2f1ae424481ee07d7e107b43fde
Lee Qi Zheng <leeqz2001@gmail.com>
2026-01-22 19:38:15 +0800

    feat Level-6: modify Quzee.java to implement delete functionality

--- src/main/java/Quzee.java
+++ src/main/java/Quzee.java
@@ -7,9 +7,17 @@ public class Quzee {
 
     private static void addTaskToDatabase(Task task) {
         database.add(task);
-        System.out.println("Got it. I've added this task:\n" + task.toString());
+        System.out.println("Got it. I've added this task:\n" + task);
         int size = database.size();
-        System.out.println("Now you have " + size + " task" + (size == 1 ? "" : "s") + " in the list.");
+        System.out.println("Now you have " + size + " task" + (size <= 1 ? "" : "s") + " in the list.");
+    }
+
+    private static void removeTaskFromDatabase(int index) {
+        Task task = database.get(index);
+        database.remove(index);
+        System.out.println("Noted. I've removed this task:\n" + task);
+        int size = database.size();
+        System.out.println("Now you have " + size + " task" + (size <= 1 ? "" : "s") + " in the list.");
     }
 
     public static void main(String[] args) {
@@ -32,7 +40,7 @@ public class Quzee {
                         System.out.println("Here are the tasks in your list:");
                         int count = 0;
                         for (Task task : database) {
-                            System.out.println(++count + "." + task.toString());
+                            System.out.println(++count + "." + task);
                         }
                     }
 
@@ -61,6 +69,22 @@ public class Quzee {
                     }
                     System.out.println(task);
 
+                } else if (userInput.startsWith("delete")) {
+
+                    String[] modifiedUserInput = userInput.split("\\s+");
+
+                    if  (modifiedUserInput.length == 1) {
+                        throw new QuzeeException("For some reason, the Task number is missing!");
+                    }
+
+                    int index = Integer.parseInt(modifiedUserInput[1]);
+
+                    if (index <= 0 || index > database.size()) {
+                        throw new QuzeeException("For some reason, I do not have Task " + index + "!");
+                    }
+
+                    removeTaskFromDatabase(index - 1);
+
                 } else if (userInput.startsWith("todo")) {
 
                     if (userInput.length() < 5) {
@@ -78,11 +102,9 @@ public class Quzee {
                         throw new QuzeeException("For some reason, \"/by <deadline>\" is missing!");
                     }
                     String[] modifiedUserInput = userInput.substring(9).split("\\s+/by\\s+");
-                    if (modifiedUserInput[0].isEmpty()) {
-                        throw new QuzeeException("For some reason, the description is missing!");
-                    }
-                    if (modifiedUserInput[1].isEmpty()) {
-                        throw new QuzeeException("For some reason, the deadline is missing!");
+                    if (modifiedUserInput.length < 2) {
+                        throw new QuzeeException("For some reason, invalid input format!\n" +
+                                "Deadline format should be: event <description> /by <deadline>");
                     }
 
                     addTaskToDatabase(new Deadline(modifiedUserInput[0], modifiedUserInput[1]));
@@ -99,14 +121,9 @@ public class Quzee {
 
                     String[] modifiedUserInput = userInput.substring(6).split("\\s+/from\\s+|\\s+/to\\s+");
 
-                    if (modifiedUserInput[0].isEmpty()) {
-                        throw new QuzeeException("For some reason, the description is missing!");
-                    }
-                    if (modifiedUserInput[1].isEmpty()) {
-                        throw new QuzeeException("For some reason, the time period (start) is missing!");
-                    }
-                    if (modifiedUserInput[2].isEmpty()) {
-                        throw new QuzeeException("For some reason, the time period (end) is missing!");
+                    if (modifiedUserInput.length < 3) {
+                        throw new QuzeeException("For some reason, invalid input format!\n" +
+                                "Event format should be: event <description> /from <start> /to <end>");
                     }
                     addTaskToDatabase(new Event(modifiedUserInput[0], modifiedUserInput[1], modifiedUserInput[2]));
 
