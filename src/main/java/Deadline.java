422ef657c400de241b38169183cee010846c5a79
Lee Qi Zheng <leeqz2001@gmail.com>
2026-02-03 14:16:30 +0800

    feat Level-8: Modify Task.java and its subclasses for date and time format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,22 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
 
-    protected String deadline;
+    protected LocalDateTime deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = LocalDateTime.parse(deadline, INPUT_FORMAT);
     }
 
     @Override
     public String convertTaskToString() {
-        return "D | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.deadline;
+        return "D | " + (this.isDone ? "1" : "0") + " | " + this.description + " | "
+                + this.deadline.format(INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+        return "[D]" + super.toString() + " (by: " + this.deadline.format(OUTPUT_FORMAT) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,25 @@
+import java.time.LocalDateTime;
+
 public class Event extends Task {
 
-    protected String start;
-    protected String end;
+    protected LocalDateTime start;
+    protected LocalDateTime end;
 
     public Event(String description, String start, String end) {
         super(description);
-        this.start = start;
-        this.end = end;
+        this.start = LocalDateTime.parse(start, INPUT_FORMAT);
+        this.end = LocalDateTime.parse(end, INPUT_FORMAT);
     }
 
     @Override
     public String convertTaskToString() {
-        return "E | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.start + "-" + this.end;
+        return "E | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.start.format(INPUT_FORMAT)
+                + "-" + this.end.format(INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
+        return "[E]" + super.toString() + " (from: " + this.start.format(OUTPUT_FORMAT) + " to: "
+                + this.end.format(OUTPUT_FORMAT) + ")";
     }
 }
