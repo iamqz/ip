d63153726cb9c16a8cc80c90f552de54a0a57344
Lee Qi Zheng <leeqz2001@gmail.com>
2026-02-05 03:04:42 +0800

    feat: implement ToDoTest tests

---
+++ src/test/java/quzee/task/ToDoTest.java
@@ -0,0 +1,27 @@
+package quzee.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+
+    @Test
+    public void test_toString_notDone_success() {
+        ToDo toDo = new ToDo("Finish CS2103");
+        assertEquals("[T][ ] Finish CS2103", toDo.toString());
+    }
+
+    @Test
+    public void test_toString_done_success() {
+        ToDo toDo = new ToDo("Finish CS2103");
+        toDo.markAsDone();
+        assertEquals("[T][X] Finish CS2103", toDo.toString());
+    }
+
+    @Test
+    public void test_convertTaskToString_success() {
+        ToDo toDo = new ToDo("Finish CS2103");
+        assertEquals("T | 0 | Finish CS2103", toDo.convertTaskToString());
+    }
+}
