20cf78d2e995712e7dbe6c4c18a2e992084bd9c9
Lee Qi Zheng <leeqz2001@gmail.com>
2026-02-12 12:42:06 +0800

    fix(Parser.java): Modify and implement the correct logics

--- src/main/java/quzee/Parser.java
+++ src/main/java/quzee/Parser.java
@@ -20,6 +20,11 @@ import quzee.task.ToDo;
  */
 public class Parser {
 
+    private static final String REGEX_SPACING = "\\s+";
+
+    private static final String MARKER_BY = "/by";
+    private static final String MARKER_FROM = "/from";
+    private static final String MARKER_TO = "/to";
     /**
      * Parses the user input and returns the corresponding Command object.
      * @param userInput The raw input string inputted by the user.
@@ -28,109 +33,99 @@ public class Parser {
      * @throws QuzeeException If user input is invalid or incomplete.
      */
     public static Command parse(String userInput, List<Task> tasksList) throws QuzeeException {
-        String[] modifiedUserInput = userInput.split("\\s+");
+        String[] modifiedUserInput = userInput.split(REGEX_SPACING, 2);
         String commandWord = modifiedUserInput[0].toLowerCase();
 
         return switch (commandWord) {
-            case "bye" -> new ExitCommand();
-            case "list" -> new ListCommand();
-            case "find" -> helperFind(modifiedUserInput);
-            case "delete" -> helperDelete(modifiedUserInput, tasksList);
-            case "mark" -> helperMark(modifiedUserInput, tasksList);
-            case "unmark" -> helperUnmark(modifiedUserInput, tasksList);
-            case "todo" -> helperTodo(modifiedUserInput);
-            case "deadline" -> helperDeadline(modifiedUserInput);
-            case "event" -> helperEvent(modifiedUserInput);
-            default -> throw new QuzeeException("Unknown command: " + commandWord);
+        case "bye" -> new ExitCommand();
+        case "list" -> new ListCommand();
+        case "find" -> helperFind(modifiedUserInput[1]);
+        case "delete" -> helperDelete(modifiedUserInput[1], tasksList);
+        case "mark" -> helperMark(modifiedUserInput[1], tasksList);
+        case "unmark" -> helperUnmark(modifiedUserInput[1], tasksList);
+        case "todo" -> helperTodo(modifiedUserInput[1]);
+        case "deadline" -> helperDeadline(modifiedUserInput[1]);
+        case "event" -> helperEvent(modifiedUserInput[1]);
+        default -> throw new QuzeeException("Unknown command: " + commandWord);
         };
     }
 
-    private static Command helperFind(String[] modifiedUserInput) throws QuzeeException {
-        if (modifiedUserInput.length != 2) {
+    private static Command helperFind(String editedInput) throws QuzeeException {
+        if (editedInput.isEmpty()) {
             throw new QuzeeException("For some reason, the keyword is missing!");
         }
-        return new FindCommand(modifiedUserInput[1].trim());
+        return new FindCommand(editedInput);
     }
 
-    private static Command helperDelete(String[] modifiedUserInput, List<Task> tasksList) throws QuzeeException {
-
-        if (modifiedUserInput.length != 2) {
+    /**
+     * Helper function to validate and convert String-coded index into int-index.
+     * Used in helperDelete, helperMark, helperUnmark.
+     * @param editedInput String-coded index.
+     * @param tasksList The list of tasks.
+     * @return int-coded index (0-indexed).
+     * @throws QuzeeException QuzeeException is thrown when input is invalid.
+     */
+    private static int validateAndParseIndex(String editedInput, List<Task> tasksList) throws QuzeeException {
+        if (editedInput.isEmpty()) {
             throw new QuzeeException("For some reason, the Task number is missing!");
         }
 
-        int index = Integer.parseInt(modifiedUserInput[1]);
+        int index = Integer.parseInt(editedInput);
         boolean isValidIndex = index > 0 && index <= tasksList.size();
 
         if (!isValidIndex) {
             throw new QuzeeException("For some reason, I do not have Task " + index + "!");
         }
-
-        return new DeleteCommand(index - 1);
+        return index - 1;
     }
 
-    private static Command helperMark(String[] modifiedUserInput, List<Task> tasksList) throws QuzeeException {
+    private static Command helperDelete(String editedInput, List<Task> tasksList) throws QuzeeException {
 
-        if (modifiedUserInput.length != 2) {
-            throw new QuzeeException("For some reason, the Task number is missing!");
-        }
-
-        int index = Integer.parseInt(modifiedUserInput[1]);
-        boolean isValidIndex = index > 0 && index <= tasksList.size();
-
-        if (!isValidIndex) {
-            throw new QuzeeException("For some reason, I do not have Task " + index + "!");
-        }
-
-        return new MarkCommand(index - 1);
+        return new DeleteCommand(validateAndParseIndex(editedInput, tasksList));
     }
 
-    private static Command helperUnmark(String[] modifiedUserInput, List<Task> tasksList) throws QuzeeException {
+    private static Command helperMark(String editedInput, List<Task> tasksList) throws QuzeeException {
 
-        if (modifiedUserInput.length != 2) {
-            throw new QuzeeException("For some reason, the Task number is missing!");
-        }
-
-        int index = Integer.parseInt(modifiedUserInput[1]);
-        boolean isValidIndex = index > 0 && index <= tasksList.size();
-
-        if (!isValidIndex) {
-            throw new QuzeeException("For some reason, I do not have Task " + index + "!");
-        }
-
-        return new UnmarkCommand(index - 1);
+        return new MarkCommand(validateAndParseIndex(editedInput, tasksList));
     }
 
-    private static Command helperTodo(String[] modifiedUserInput) throws QuzeeException {
+    private static Command helperUnmark(String editedInput, List<Task> tasksList) throws QuzeeException {
 
-        if (modifiedUserInput.length != 2) {
+        return new UnmarkCommand(validateAndParseIndex(editedInput, tasksList));
+    }
+
+    private static Command helperTodo(String editedInput) throws QuzeeException {
+
+        if (editedInput.isEmpty()) {
             throw new QuzeeException("For some reason, the description is missing!");
         }
 
-        return new AddCommand(new ToDo(modifiedUserInput[1]));
+        return new AddCommand(new ToDo(editedInput));
     }
 
-    private static Command helperDeadline(String[] modifiedUserInput) throws QuzeeException {
+    private static Command helperDeadline(String editedInput) throws QuzeeException {
 
-        boolean isValidInput = modifiedUserInput.length == 4 && modifiedUserInput[2].equals("/by");
+        String[] temp = editedInput.split(MARKER_BY, 2);
+        boolean isValidInput = temp.length == 2 && !temp[0].isEmpty() && !temp[1].isEmpty();
 
         if (!isValidInput) {
             throw new QuzeeException("For some reason, invalid input format!\n"
-                    + "Deadline format should be: deadline <description> /by <deadline>\nDate Format: "
+                    + "Deadline format should be: deadline <description> " + MARKER_BY + " <deadline>\nDate Format: "
                     + Task.INPUT_FORMAT_STRING);
         }
-        return new AddCommand(new Deadline(modifiedUserInput[1], modifiedUserInput[3]));
+        return new AddCommand(new Deadline(temp[0].strip(), temp[1].strip()));
     }
 
-    private static Command helperEvent(String[] modifiedUserInput) throws QuzeeException {
+    private static Command helperEvent(String editedInput) throws QuzeeException {
 
-        boolean isValidInput = modifiedUserInput.length == 6 && modifiedUserInput[2].equals("/from")
-                && modifiedUserInput[4].equals("/to");
+        String[] temp = editedInput.split(MARKER_FROM + "|" + MARKER_TO, 2);
+        boolean isValidInput = temp.length == 3 && !temp[0].isEmpty() && !temp[1].isEmpty() && !temp[2].isEmpty();
 
         if (!isValidInput) {
             throw new QuzeeException("For some reason, invalid input format!\n"
-                    + "Event format should be: event <description> /from <start> /to <end>\nDate Format: "
-                    + Task.INPUT_FORMAT_STRING);
+                    + "Event format should be: event <description> " + MARKER_FROM + " <start> " + MARKER_TO
+                    + " <end>\nDate Format: " + Task.INPUT_FORMAT_STRING);
         }
-        return new AddCommand(new Deadline(modifiedUserInput[1], modifiedUserInput[3]));
+        return new AddCommand(new Event(temp[0].strip(), temp[1].strip(), temp[2].strip()));
     }
 }
